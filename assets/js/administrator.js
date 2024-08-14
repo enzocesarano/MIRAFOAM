687804ce1189b3d45dade64f52c401ddb0be92fd
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-08-14 15:56:02 +0200

    up

--- assets/js/administrator.js
+++ assets/js/administrator.js
@@ -21,6 +21,7 @@ const inputCatalog = document.getElementById('inputCatalog');
 const formCard = document.getElementById('formCard');
 const inputSearch = document.getElementById('inputSearch');
 const rowDetails = document.getElementById('rowDetails');
+const search = document.getElementById('search')
 let products = [];
 let existingRelatedImages = []; // Variabile per tenere traccia delle immagini esistenti
 
@@ -29,19 +30,21 @@ window.addEventListener('scroll', function () {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     if (scrollTop > 100) {
         nav.style.background = 'white';
-        nav.classList.remove('navbar-dark');
-        inputSearch.classList.add('input-dark');
+        nav.classList.remove('navbar-dark')
+        inputSearch.classList.add('input-dark')
         cls2.forEach(element => {
-            element.style.fill = '#343a40';
-        });
-        inputSearch.classList.add('inputBlack', 'bg-secondary');
+            element.style.fill = '#343a40'
+        })
+        inputSearch.classList.add('inputBlack', 'bg-secondary')
+        search.classList.add('btn-outline-dark')
     } else {
-        nav.style.background = 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0))';
+        nav.style.background = 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0)';
         cls2.forEach(element => {
-            element.style.fill = 'white';
-        });
-        nav.classList.add('navbar-dark');
-        inputSearch.classList.remove('inputBlack', 'bg-secondary');
+            element.style.fill = 'white'
+        })
+        nav.classList.add('navbar-dark')
+        inputSearch.classList.remove('inputBlack', 'bg-secondary')
+        search.classList.remove('btn-outline-dark')
     }
 });
 
